81d1c696b99ce58a3e0b6f72dd2ce6719faaa0e7
TommasoBrichetti <106075600+TommasoBrichetti@users.noreply.github.com>
2022-06-07 15:02:56 +0200

    Add files via upload

---
+++ W3D2_progetto/scrypt.js
@@ -0,0 +1,57 @@
+let nome = "Mirtilla";
+var posizione = "Cliente"
+const piGreco = 3.14;
+
+console.log(nome);
+console.log(posizione);
+console.log(piGreco);
+
+nome = "Lilla"
+posizione = "Amministratore";
+
+console.log(nome);
+console.log(posizione);
+
+let lavoro01 = "Developer";
+let lavoro02 = "Formazione";
+console.log("Io mi occupo di: " +lavoro01+ " e " +lavoro02);
+
+let professione = lavoro01 +" e " +lavoro02;
+console.log("Io mi occupo di: "+ professione);
+
+
+let booleana = true;
+console.log("Let JS:" + booleana);
+
+let nondefinita;
+console.log(nondefinita);
+
+let anni = 30;
+console.log(anni);
+console.log("Lilla ha " + anni + "anni");
+
+anni = 15;
+console.log(anni);
+
+anni -= 4;
+console.log(anni);
+
+anni += 3;
+console.log(anni);
+
+
+let nomePet = "Billone";
+let colorePet = "Rosso";
+console.log("Il nome del pet: " +nomePet+ "ed il suo colore è: " +colorePet);
+
+let animale = nomePet+ "ed il suo colore è: " +colorePet;
+console.log("Il nome del pet: " +animale);
+
+
+console.log("string\ncon righe\nmultiple");
+console.log("string\ncon righe\nmultiple");
+
+
+
+
+
