9821d943872cbce81fc7f7ebcc94083ec39619e7
TommasoBrichetti <106075600+TommasoBrichetti@users.noreply.github.com>
2022-06-10 11:27:59 +0200

    Add files via upload

---
+++ W3_Progetto/script.js
@@ -0,0 +1,16 @@
+var base = "";
+document.getElementById("schermino").value = base;
+
+function aggiungi(n) {
+    document.getElementById("schermino").value = document.getElementById("schermino").value+n;
+    document.getElementById("schermino").innerHTML = document.getElementById("schermino").value;
+}
+function cancella(){
+    document.getElementById("schermino").value = base;
+    document.getElementById("schermino").innerHTML = base;
+}
+function esegui(){
+    var risultato = eval(document.getElementById("schermino").value);
+    document.getElementById("schermino").value = risultato;
+    document.getElementById("schermino").innerHTML = risultato;
+}
